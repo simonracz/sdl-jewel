5d37c2510581d3336d0d5b231f4849fe3d3fd923
Simon Racz <simonracz@gmail.com>
2014-03-23 23:45:20 +0100

    Simple SDL app works.

--- sdl-jewel/main.cpp
+++ sdl-jewel/main.cpp
@@ -7,12 +7,20 @@
 //
 
 #include <iostream>
+#include "SDL2/SDL.h"
+#include "SDL2_image/SDL_image.h"
 
-int main(int argc, const char * argv[])
+int main( int argc, char* args[] )
 {
-
-	// insert code here...
-	std::cout << "Hello, World!\n";
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	
+    //Quit SDL
+	SDL_Quit();
+    
+	std::cout << "Done" << std::endl;
     return 0;
 }
 
