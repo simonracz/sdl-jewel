75a83f47d48241ae200ffefca3f90e3395b9fe32
Simon Racz <simonracz@gmail.com>
2014-03-23 23:39:58 +0100

    Initial Commit

---
+++ sdl-jewel/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sdl-jewel
+//
+//  Created by Simon Racz on 23/03/14.
+//  Copyright (c) 2014 Simon Racz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
